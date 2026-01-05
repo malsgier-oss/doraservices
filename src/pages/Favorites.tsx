8f5e8f0a876abd31b36d05268e182bb64b825efb
malsgier-oss <m.alsgier@gmail.com>
2026-01-05 13:14:01 -0800

    Update Favorites.tsx

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -157,7 +157,7 @@ export default function Favorites() {
 
   if (loading) {
     return (
-      <Layout>
+      <Layout hideHeader>
         <div className="min-h-screen flex items-center justify-center">
           <Loader2 className="h-8 w-8 animate-spin text-primary" />
         </div>
