d3ce751abefc3d76b7e9af598e8d0efa585213f4
malsgier-oss <m.alsgier@gmail.com>
2026-01-13 18:09:14 +0200

    src/main.tsx   (fully updated)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("CF_DEPLOY_TEST_01");
+
 createRoot(document.getElementById("root")!).render(<App />);
